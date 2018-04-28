37e6cc69a17bbe0e178add85c7a5abc5366530b0
ronysilva <ronysilvati@Live.com>
2018-04-28 20:11:50 -0300

    Adicionada a aplicação "MinhaCalculadora" com Testes passando

---
+++ src/devops_1/CalculadoraString.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devops_1;
+
+/**
+ *
+ * @author visitante
+ */
+public class CalculadoraString {
+
+    public CalculadoraString() {
+
+    }
+
+    public String adicionar(String numero) {
+        String valorRetornado = numero;
+        String[] valoresDaAdicao;
+        int somaValores = 0;
+        String delimitadorOriginal = ",";
+        String delimitadorTemp = "_";
+        String mensagemSistema = "";
+        String mensagemNegativos = "";
+
+        if (numero == "") {
+            valorRetornado = "0";
+        } else {
+            if ((numero.length() > 5) && numero.substring(0, 2).equals("//")) {
+
+                delimitadorOriginal = this.getDelimitador(numero);
+                numero = numero.replace("//" + delimitadorOriginal + "\n", "");
+
+                if (delimitadorOriginal.equals("|")) {
+                    numero = numero.replace(delimitadorOriginal, delimitadorTemp);
+                } else {
+                    delimitadorTemp = delimitadorOriginal;
+                }
+            } else {
+                delimitadorTemp = delimitadorOriginal;
+            }
+
+            if (numero.substring(numero.length() - 1).equals(delimitadorTemp)) {
+                return "\nNúmero esperado mas EOF encontrado";
+            }
+
+            if ((delimitadorTemp != ",") && (numero.indexOf(",") > -1)) {
+                mensagemSistema += "\n'" + delimitadorOriginal + "' esperado mas ',' encontrado na posição " + (numero.indexOf(","));
+                valorRetornado = mensagemSistema;
+            } else {
+
+                if (numero.indexOf(delimitadorTemp + delimitadorTemp) > -1) {
+                    mensagemSistema += "\nNúmero esperado mas " + delimitadorTemp + " encontrado na posição " + (numero.indexOf(delimitadorTemp + delimitadorTemp));
+                }
+
+                if (numero.indexOf(delimitadorTemp + "\n") > -1) {
+                    mensagemSistema += "\nNúmero esperado mas \\n encontrado na posição " + (numero.indexOf(delimitadorTemp + "\n") + 1);
+                    valorRetornado = mensagemSistema;
+                } else {
+
+                    valoresDaAdicao = numero.split(delimitadorTemp + "|\n");
+
+                    for (String valorAdicao : valoresDaAdicao) {
+
+                        if (!valorAdicao.isEmpty()) {
+
+                            if (Integer.parseInt(valorAdicao) < 0) {
+
+                                if (mensagemNegativos != "") {
+                                    mensagemNegativos += ", ";
+                                }
+
+                                mensagemNegativos += valorAdicao;
+
+                            } else if (Integer.parseInt(valorAdicao) < 9999) {
+                                somaValores += Integer.parseInt(valorAdicao);
+                            }
+                        }
+
+                    }
+
+                    if (mensagemNegativos != "") {
+                        mensagemSistema += "\nNúmero negativo não permitido: " + mensagemNegativos;
+                        valorRetornado = mensagemSistema;
+                    } else {
+                        valorRetornado = Integer.toString(somaValores);
+                    }
+                }
+
+            }
+
+            
+        }
+        
+        return valorRetornado;
+    }
+
+    protected String getDelimitador(String numero) {
+        int indexQuebraLinha = numero.indexOf('\n');
+        return numero.substring(2, indexQuebraLinha);
+    }
+
+}
---
+++ test/devops_1/CalculadoraStringTest.java
@@ -0,0 +1,183 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devops_1;
+
+import devops_1.CalculadoraString;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author visitante
+ */
+public class CalculadoraStringTest {
+    
+    public CalculadoraStringTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    
+    @Test
+    public void testAdicionarFuncaoAdicionarSemParametros(){
+        CalculadoraString cs = new CalculadoraString();
+        
+        
+        assertEquals("0", cs.adicionar(""));
+    }
+    
+    
+    @Test
+    public void testAdicionarFuncaoAdicionarUmParametro(){
+        CalculadoraString cs = new CalculadoraString();
+        
+        
+        assertEquals("2", cs.adicionar("2"));
+    }
+    
+    @Test
+    public void testAdicionarFuncaoAdicionarDoisParametros(){
+        CalculadoraString cs = new CalculadoraString();
+        
+        
+        assertEquals("5", cs.adicionar("2,3"));
+    }
+    
+    @Test
+    public void testAdicionarFuncaoAdicionarTresParametros(){
+        CalculadoraString cs = new CalculadoraString();
+        
+        
+        assertEquals("10", cs.adicionar("2,3,5"));
+    }
+    
+    
+    @Test
+    public void testAdicionarFuncaoAdicionarQuatroParametros(){
+        CalculadoraString cs = new CalculadoraString();
+        
+        
+        assertEquals("20", cs.adicionar("2,3,5,10"));
+    }
+    
+    @Test
+    public void testAdicionarFuncaoAdicionarNovoSeparador(){
+        CalculadoraString cs = new CalculadoraString();
+        
+        
+        assertEquals("6", cs.adicionar("1\n2,3"));
+    }
+    
+    @Test
+    public void testAdicionarFuncaoAdicionarNovoSeparadorPosicaoErrada(){
+        CalculadoraString cs = new CalculadoraString();
+        
+        
+        assertEquals("\nNúmero esperado mas \\n encontrado na posição 2", cs.adicionar("1,\n3"));
+    }
+    
+    @Test
+    public void testAdicionarFuncaoAdicionarNumeroFaltando(){
+        CalculadoraString cs = new CalculadoraString();
+        
+        
+        assertEquals("\nNúmero esperado mas EOF encontrado", cs.adicionar("1,3,"));
+    }
+    
+    @Test
+    public void testAdicionarFuncaoAdicionarNovoDelimitador(){
+        CalculadoraString cs = new CalculadoraString();
+        
+        
+        assertEquals("3", cs.adicionar("//;\n1;2"));
+    }
+    
+    
+    @Test
+    public void testAdicionarFuncaoAdicionarNovoDelimitador2(){
+        CalculadoraString cs = new CalculadoraString();
+        
+        
+        assertEquals("6", cs.adicionar("//|\n1|2|3"));
+    }
+    
+    @Test
+    public void testAdicionarFuncaoAdicionarNovoDelimitador3(){
+        CalculadoraString cs = new CalculadoraString();
+        
+        
+        assertEquals("6", cs.adicionar("//sep\n1sep2sep3"));
+    }
+    
+    
+    @Test
+    public void testAdicionarFuncaoAdicionarNovoDelimitadorErradoPosicao(){
+        CalculadoraString cs = new CalculadoraString();
+        
+        
+        assertEquals("\n'|' esperado mas ',' encontrado na posição 3", cs.adicionar("//|\n1|2,3"));
+    }
+    
+    
+    @Test
+    public void testAdicionarNegativos(){
+        CalculadoraString cs = new CalculadoraString();
+        
+        
+        assertEquals("\nNúmero negativo não permitido: -1", cs.adicionar("-1,2"));
+    }
+    
+    
+    @Test
+    public void testAdicionarMultiplosErros(){
+        CalculadoraString cs = new CalculadoraString();
+        
+        
+        assertEquals("\nNúmero esperado mas , encontrado na posição 2\nNúmero negativo não permitido: -2", cs.adicionar("-2,,2"));
+    }
+
+
+    @Test
+    public void testAdicionarNumerosGrandes(){
+        CalculadoraString cs = new CalculadoraString();
+        
+        
+        assertEquals("2", cs.adicionar("2,99123"));
+    }
+
+
+}
