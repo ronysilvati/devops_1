efac33c4a7075f43dedd37f5a2890cba86a90e3c
Rony S. <ronysilvati@live.com>
2018-05-03 23:44:49 -0300

    Adicionado gradle

--- src/main/java/CalculadoraString.java
+++ src/main/java/CalculadoraString.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package devops_1;
+package main.java;
 
 /**
  *
--- test/java/CalculadoraStringTest.java
+++ test/java/CalculadoraStringTest.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package devops_1;
+package java;
 
-import devops_1.CalculadoraString;
+import main.java.CalculadoraString;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
